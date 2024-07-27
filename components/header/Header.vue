5e49a26f127e64b8e18a8f428237518c46290105
DungeonmaAsster <kolya.kotlyarenko@gmail.com>
2024-07-27 16:25:18 +0500

    first commit

---
+++ components/header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="header__container">
+        <div class="header__logo-container">
+            <logo class="header__logo"/>
+            <button class="header__category-button">Все категории</button>
+        </div>
+
+        <div class="header__info-container">
+            <div>Ростов-на-Дону</div>
+            <a class="header__offers-link" href="/offers">Разместить обьявление</a>
+            <button class="header__info-button">
+                <BurgerIcon class="header__burger-icon" />
+                <img src="~/assets/images/header/avatar-placeholder.png" alt="User avatar" class="header__user-avatar" />
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import Logo from "~/assets/images/header/Logo.vue";
+import BurgerIcon from "~/assets/images/header/BurgerIcon.vue";
+
+export default {
+    components: {
+        Logo,
+        BurgerIcon
+    },
+}
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/index.scss";
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="main-wrapper">
+        <Header/>
+        <div>
+            <slot />
+        </div>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Footer from "~/components/footer/Footer.vue";
+import Header from "~/components/header/Header.vue";
+
+export default {
+  components: {
+    Footer,
+    Header
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.main-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+</style>
